08f8825841c6c63678628b1fbe9bafcf512fcc8e
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-03-21 14:47:46 +0500

    Fix linter

--- Gemfile
+++ Gemfile
@@ -1,3 +1,3 @@
 source 'https://rubygems.org'
-gem 'rubocop', '>= 1.0', '< 2.0'
 gem 'rspec', '~> 3.10'
+gem 'rubocop', '>= 1.0', '< 2.0'
--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -1,14 +1,14 @@
 require_relative '../classes/genre'
 
 describe Genre do
-    let(:genre) { Genre.new('Rock') }
-    
-    it 'has a name' do
-        expect(genre.name).to eq('Rock')
-    end
-    it 'testing of add_item' do
-        item = Item.new('Rock', 'author', 'source', 'label', 'date')
-        genre.add_item(item)
-        expect(genre.item).to include(item)
-    end
+  let(:genre) { Genre.new('Rock') }
+
+  it 'has a name' do
+    expect(genre.name).to eq('Rock')
+  end
+  it 'testing of add_item' do
+    item = Item.new('Rock', 'author', 'source', 'label', 'date')
+    genre.add_item(item)
+    expect(genre.item).to include(item)
+  end
 end
