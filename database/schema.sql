a6c46f36f9fd5a27cd7192a14e319f3f31c70e67
Roy Jumah <royjumah17@gmail.com>
2023-03-22 19:08:19 +0300

    Add schema for label and book classes

--- database/schema.sql
+++ database/schema.sql
@@ -9,3 +9,17 @@ create table music_album (
     publish_date date not null,
     on_spotify boolean
 );
+
+
+CREATE TABLE book (
+    publisher VARCHAR(255),
+    cover_state VARCHAR(255)
+);
+
+
+CREATE TABLE label (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    color VARCHAR(255),
+    items ARRAY
+);
